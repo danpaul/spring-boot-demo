8726968ea9e7cdce24f9a22769b381f293492708
dan-inholland <daniel.breczinski@inholland.nl>
2024-04-16 16:40:49 +0200

    fix direct repo call

--- src/main/java/com/breczinski/webapitest/controller/TransactionController.java
+++ src/main/java/com/breczinski/webapitest/controller/TransactionController.java
@@ -2,20 +2,24 @@ package com.breczinski.webapitest.controller;
 
 import com.breczinski.webapitest.models.Transaction;
 import com.breczinski.webapitest.repository.TransactionRepository;
+import com.breczinski.webapitest.service.TransactionService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.crypto.dsig.TransformService;
 import java.util.ArrayList;
 
 @RestController
 public class TransactionController {
 
-    TransactionRepository transactionRepository;
-    TransactionController(TransactionRepository t){
-        transactionRepository = t;
+    TransactionService transactionService;
+    TransactionController(TransactionService t){
+        transactionService = t;
     }
     @GetMapping("/transactions")
+    @ResponseBody
     public ArrayList<Transaction> transactions() {
-        return transactionRepository.GetAll();
+        return transactionService.GetAll();
     }
 }
